368608439f87659eb83bafbf3fa5398d47947b7d
Apocalipsis117 <74636100+Apock-graficos@users.noreply.github.com>
2025-04-03 11:40:49 -0500

    nuevo vs

--- script.js
+++ script.js
@@ -69,6 +69,12 @@ new Vue({
                 img: 'malzahar.jpg',
                 win: [],
             },
+            {
+                id: 12,
+                name: 'delicaso',
+                img: 'caitlyn.jpg',
+                win: [],
+            },
         ]
     }
 })
